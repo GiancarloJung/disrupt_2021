a75a3bffcdb71305b40496ac7099ec834d36088f
Giancarlo Jung <dev@addicted.coffee>
2020-11-15 19:17:25 -0300

    Adds SingletonMeta to Database metaclass

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -1,13 +1,15 @@
 import os
 import sqlalchemy as db
 
+from app.libs.singleton.singleton_meta import SingletonMeta
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import scoped_session, sessionmaker
 
 
 Base = declarative_base()
 
-class Database():
+class Database(metaclass=SingletonMeta):
     engine = db.create_engine(os.environ['DATABASE_URL'])
     session = None
 
@@ -16,4 +18,4 @@ class Database():
         self.connection = self.engine.connect()
         Base.metadata.create_all(self.engine)
 
-        print("DB Instance Created")
+        print(f'{self}: DB Instance Created')
