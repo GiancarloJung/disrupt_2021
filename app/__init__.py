ee4a75195b7351d4adcab10160293df7e5abe145
Giancarlo Jung <dev@addicted.coffee>
2020-11-15 19:20:55 -0300

    Adds basic layout for Dashboard page

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,22 @@
 import streamlit as st
 from app.db import Database
+import awesome_streamlit as ast
+from app.pages import dashboard
 
 
-database = Database()
+PAGES = {
+    "Dashboard": dashboard
+}
 
 def run():
-    st.title("Dsrpt21")
+    st.set_page_config(layout="wide")
+
+    st.sidebar.title("Navigation")
+    selection = st.sidebar.radio("Go to", list(PAGES.keys()))
+
+    st.sidebar.button("Logout")
+
+    page = PAGES[selection]
+
+    with st.spinner(f"Loading..."):
+        ast.shared.components.write_page(page)
